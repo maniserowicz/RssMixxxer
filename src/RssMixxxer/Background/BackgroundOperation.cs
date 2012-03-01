ecc4df0817f0e6eaf40dbeb280ef829225693fbc
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2012-03-01 20:51:44 +0100

    added background operation runner

---
+++ src/RssMixxxer/Background/BackgroundOperation.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+using NLog;
+using Timer = System.Timers.Timer;
+
+namespace RssMixxxer.Background
+{
+    /// <summary>
+    /// Non-reentrant
+    /// </summary>
+    public class BackgroundOperation : IDisposable
+    {
+        private readonly Action _action;
+        private Timer _timer;
+        private volatile int _counter;
+        private object _syncRoot = new object();
+        private volatile bool _isStarted;
+        private volatile bool _isDisposed;
+
+        public BackgroundOperation(Action action, TimeSpan interval)
+        {
+            _action = action;
+            _timer = new Timer(interval.TotalMilliseconds)
+                {
+                    AutoReset = false,
+                };
+            _timer.Elapsed += (_, __) => Execute();
+        }
+
+        public void Start()
+        {
+            lock (_syncRoot)
+            {
+                if (_isStarted)
+                {
+                    throw new InvalidOperationException("Operation already started");
+                }
+                _isStarted = true;
+            }
+
+            Execute();
+        }
+
+        private void Execute()
+        {
+            ThreadPool.QueueUserWorkItem(_ =>
+                {
+                    if (_isDisposed)
+                    {
+                        return;
+                    }
+
+                    try
+                    {
+                        _counter++;
+
+                        _log.Debug("Executing background job, iteration counter: {0}", _counter);
+
+                        _action();
+                    }
+                    catch (Exception exc)
+                    {
+                        _log.ErrorException("Unhandled error when executing backround operation", exc);
+                    }
+                    finally
+                    {
+                        if (_isDisposed == false && _timer != null)
+                        {
+                            _log.Debug("Scheduling background job to run in {0}", TimeSpan.FromMilliseconds(_timer.Interval).ToString());
+
+                            _timer.Start();
+                        }
+                    }
+                });
+        }
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                throw new ObjectDisposedException("BackgroundOperation");
+            }
+
+            _isDisposed = true;
+            if (_timer != null)
+            {
+                _timer.Stop();
+                _timer.Dispose();
+                _timer = null;
+            }
+        }
+
+        private static readonly Logger _log = LogManager.GetCurrentClassLogger();
+    }
+}
