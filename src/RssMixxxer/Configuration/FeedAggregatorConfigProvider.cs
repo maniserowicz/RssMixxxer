44d61ffeb9f97706dd6115e8507c49fba052ad19
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2012-03-01 22:57:47 +0100

    fix in config provider - incorrect app setting was read

--- src/RssMixxxer/Configuration/FeedAggregatorConfigProvider.cs
+++ src/RssMixxxer/Configuration/FeedAggregatorConfigProvider.cs
@@ -16,7 +16,7 @@ namespace RssMixxxer.Configuration
             return new FeedAggregatorConfig
                 {
                     Title = appSettings["rssMixxxer.title"],
-                    MaxItems = int.Parse(appSettings["rssMixxxer.title"]),
+                    MaxItems = int.Parse(appSettings["rssMixxxer.maxItems"]),
                     SourceFeeds = appSettings["rssMixxxer.src"].Split(';'),
                     SyncInterval_Seconds = int.Parse(appSettings["rssMixxxer.interval_seconds"]),
                 };
