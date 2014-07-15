b67421dbeb8346c11bec09f3ba74b6e8c2877907
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2014-07-15 23:30:49 +0200

    added PrefetchHeadRequest config entry that enables this new feature

--- src/RssMixxxer/Configuration/FeedAggregatorConfig.cs
+++ src/RssMixxxer/Configuration/FeedAggregatorConfig.cs
@@ -6,5 +6,6 @@
         public int MaxItems { get; set; }
         public string[] SourceFeeds { get; set; }
         public int SyncInterval_Seconds { get; set; }
+        public bool PrefetchHeadRequest { get; set; }
     }
 }
